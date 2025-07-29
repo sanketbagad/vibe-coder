06bf4e73997ed47266a4445b65a28d0ed207ec5d
Sanket Bagad <alencolins@gmail.com>
2025-07-29 10:50:57 +0530

    messages ui

--- src/modules/projects/ui/components/message-form.tsx
+++ src/modules/projects/ui/components/message-form.tsx
@@ -1,10 +1,116 @@
+"use client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import TextareaAutosize from "react-textarea-autosize";
+import { Form, FormField } from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+import { ArrowUpIcon, Loader2Icon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
+const formSchema = z.object({
+  value: z
+    .string()
+    .min(1, "Message cannot be empty")
+    .max(10000, "Message is too long"),
+});
 
 export function MessageForm({ projectId }: any) {
-  // implementation
+  const [isFocused, setIsFocused] = useState(false);
+  const showUsage = false; // Replace with actual logic to determine if usage should be shown
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      value: "",
+    },
+  });
+
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+
+  const createMessage = useMutation(trpc.messages.create.mutationOptions({
+    onSuccess: () => {
+      form.reset();
+      queryClient.invalidateQueries(
+        trpc.messages.getMany.queryOptions({ projectId })
+      );
+    },
+
+    onError: (error) => {
+      toast.error(`Error creating message: ${error.message}`, {
+        description: "Please try again later.",
+      });
+    },
+  }));
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    // Call the create message mutation here
+    await createMessage.mutateAsync({ value: data.value, projectId });
+  };
+
+  const isPending = createMessage.isPending;
+  const isDisabled = isPending || !form.formState.isValid;
   return (
-    <div>
-        Sanket
-    </div>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className={cn(
+          "relative border p-4 pt-1 rounded-xl bg-sidebar dark:bg-sidebar transition-all",
+          isFocused && "shadow-xs",
+          showUsage && "rounded-t-none"
+        )}
+      >
+        <FormField
+          control={form.control}
+          name="value"
+          render={({ field }) => (
+            <TextareaAutosize
+              disabled={isPending}
+              {...field}
+              minRows={2}
+              maxRows={8}
+              placeholder="What would you like to build??"
+              className="pt-4 resize-none border-none w-full outline-none bg-transparent "
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+              onKeyDown={(e) => {
+                if ((e.key === "Enter" && !e.ctrlKey) || !e.metaKey) {
+                  e.preventDefault();
+                  form.handleSubmit(onSubmit)(e);
+                }
+              }}
+            />
+          )}
+        />
+
+        <div className="flex gap-2 items-end justify-end pt-2">
+          <div className="text-[10px] text-muted-foreground font-mono">
+            <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground">
+              <span>#8984</span> Enter
+            </kbd>
+            &nbsp;to submit
+          </div>
+          <Button
+            disabled={isDisabled}
+            className={cn(
+              "size-8 rounded-full",
+              isDisabled && "bg-muted-foreground border"
+            )} type="submit">
+              {
+                isPending ? (
+                  <Loader2Icon className="size-4 animate-spin" />
+                ) : (
+                   <ArrowUpIcon className="size-4" />
+                )
+              }
+          
+          </Button>
+        </div>
+      </form>
+    </Form>
   );
 }
--- src/modules/projects/ui/components/messages-container.tsx
+++ src/modules/projects/ui/components/messages-container.tsx
@@ -1,7 +1,10 @@
+"use client";
+
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import MessageCard from "./message-card";
 import { MessageForm } from "./message-form";
+import { useEffect, useRef } from "react";
 
 interface MessagesContainerProps {
   projectId: string;
@@ -9,9 +12,23 @@ interface MessagesContainerProps {
 
 export const MessagesContainer = ({ projectId }: MessagesContainerProps) => {
   const trpc = useTRPC();
+  const bottomRef = useRef<HTMLDivElement>(null);
   const { data: messages } = useSuspenseQuery(
     trpc.messages.getMany.queryOptions({ projectId })
   );
+
+  useEffect(() => {
+    const lastAssistantMessage = messages.findLast(
+      (message) => message.role === "ASSISTANT"
+    );
+    if (lastAssistantMessage) {
+    }
+
+  }, [messages]);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages.length]);
   return (
     <div className="flex flex-col flex-1 min-h-0">
       <div className="flex-1 min-h-0 overflow-y-auto">
@@ -28,8 +45,11 @@ export const MessagesContainer = ({ projectId }: MessagesContainerProps) => {
             />
           ))}
         </div>
+        <div ref={bottomRef} className="h-4" />
       </div>
       <div className="relative p-3 pt-1">
+        <div className="absolute -top-6 left-0 right-0 h-6 bg-gradient-to-b from-transparent to-background/70 pointer-events-none" />
+        {/* Message Form */}
         <MessageForm projectId={projectId} />
       </div>
     </div>
