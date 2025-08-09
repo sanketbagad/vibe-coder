ab40229f4d22a60c3f9a237fdefb3e548aeaa241
Sanket Bagad <alencolins@gmail.com>
2025-08-09 11:38:59 +0530

    Added Custom Hook for dark made and integrated it

--- src/components/user-control.tsx
+++ src/components/user-control.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { useCurrentTheme } from "@/hooks/use-current-theme";
 import { UserButton } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 interface UserControlProps {
   showName?: boolean;
 }
 
 export const UserControl = ({ showName }: UserControlProps) => {
+    const currentTheme = useCurrentTheme();
   return (
     <UserButton
       showName={showName}
@@ -16,6 +19,7 @@ export const UserControl = ({ showName }: UserControlProps) => {
           userButtonAvatarBox: "rounded-md! size-8",
           userButtonTrigger: "rounded-md!",
         },
+        baseTheme: currentTheme === "dark" ? dark : undefined,
       }}
     />
   );
