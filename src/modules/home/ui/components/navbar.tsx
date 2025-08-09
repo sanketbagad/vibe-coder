f1bf1028ce731068080aea772a302b9f27c5d6ec
Sanket Bagad <alencolins@gmail.com>
2025-08-09 11:11:11 +0530

    Navbar before sign up designed

--- src/modules/home/ui/components/navbar.tsx
+++ src/modules/home/ui/components/navbar.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -7,7 +9,7 @@ export const Navbar = () => {
   return (
     <nav
       className="
-  p-4 bg-transparent fixed top-0 left-0 reght-0 z-50 transition-all duration-200 border-b border-transparent
+  p-4 bg-transparent fixed top-0 left-0 right-0 z-50 transition-all duration-200 border-b border-transparent
   "
     >
       <div className="max-w-5xl mx-auto w-full flex justify-between items-center">
@@ -20,6 +22,25 @@ export const Navbar = () => {
           />
           <span className="text-lg font-semibold">Botbyte.Build</span>
         </Link>
+        <SignedOut>
+          <div className="flex gap-2">
+            <SignUpButton>
+              <Button variant="outline" size="sm">
+                Sign Up
+              </Button>
+            </SignUpButton>
+            <SignInButton>
+              <Button size="sm">
+                Sign In
+              </Button>
+            </SignInButton>
+          </div>
+        </SignedOut>
+        <SignedIn>
+            <p>
+                Todo: Add user profile dropdown
+            </p>
+        </SignedIn>
       </div>
     </nav>
   );
