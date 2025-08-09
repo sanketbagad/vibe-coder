fee6c8367c75a09a08abf4a7f006a1586012bb43
Sanket Bagad <alencolins@gmail.com>
2025-08-09 11:03:37 +0530

    Clerk setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "next-themes";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Botbyte.ui",
+  title: "Botbyte.build",
   description: "Build with Botbyte AI",
 };
 
@@ -26,22 +27,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <TRPCReactProvider>
-      <html lang="en" suppressHydrationWarning>
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <ThemeProvider
-            attribute={"class"}
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
+    <ClerkProvider>
+      <TRPCReactProvider>
+        <html lang="en" suppressHydrationWarning>
+          <body
+            className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           >
-            <Toaster />
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </TRPCReactProvider>
+            <ThemeProvider
+              attribute={"class"}
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+            >
+              <Toaster />
+              {children}
+            </ThemeProvider>
+          </body>
+        </html>
+      </TRPCReactProvider>
+    </ClerkProvider>
   );
 }
---
+++ src/modules/home/ui/components/navbar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+export const Navbar = () => {
+  return (
+    <nav
+      className="
+  p-4 bg-transparent fixed top-0 left-0 reght-0 z-50 transition-all duration-200 border-b border-transparent
+  "
+    >
+      <div className="max-w-5xl mx-auto w-full flex justify-between items-center">
+        <Link href={"/"} className="flex items-center gap-2">
+          <Image
+            src="/bot.svg"
+            alt="Botbyte.Build Logo"
+            width={24}
+            height={24}
+          />
+          <span className="text-lg font-semibold">Botbyte.Build</span>
+        </Link>
+      </div>
+    </nav>
+  );
+};
